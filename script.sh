e233aa4da86b5f6f152336207280c9a9b9493f22
Trong Dinh <trongdth@gmail.com>
2017-02-23 11:17:48 +0700

    add script.sh

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+TRAVIS_PULL_REQUEST = false
+TRAVIS_PULL_REQUEST_BRANCH = "master"
+export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
+if [[${BRANCH:0:2}] == "ma"]; then
+  echo 'build -> master'
+  xcodebuild -workspace TravisTesting/TravisTesting.xcworkspace -scheme TravisTesting -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO | xcpretty
+  xcodebuild test -workspace TravisTesting/TravisTesting.xcworkspace -scheme TravisTestingTests -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.3' -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO | xcpretty
+
+fi
