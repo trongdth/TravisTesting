a42a8071e8dc4202337245e13cda0842241ece5a
Trong Dinh <trongdth@gmail.com>
2017-02-23 12:05:19 +0700

    Add UI testing schema

--- script.sh
+++ script.sh
@@ -6,5 +6,6 @@ if [[${BRANCH:0:2}] == "ma"]; then
   echo 'build -> master'
   xcodebuild -workspace TravisTesting/TravisTesting.xcworkspace -scheme TravisTesting -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO | xcpretty
   xcodebuild test -workspace TravisTesting/TravisTesting.xcworkspace -scheme TravisTestingTests -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.3' -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO | xcpretty
+  xcodebuild test -workspace TravisTesting/TravisTesting.xcworkspace -scheme TravisTestingUITests -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.3' -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO | xcpretty
 
 fi
