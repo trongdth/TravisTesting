bbd9ede332f6cfdb5e4fedfe36f1dc4b249798cf
Trong Dinh <trongdth@gmail.com>
2017-02-23 12:35:50 +0700

    Fix run shell script

--- script.sh
+++ script.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
-TRAVIS_PULL_REQUEST = false
-TRAVIS_PULL_REQUEST_BRANCH = "master"
-export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
-if [[${BRANCH:0:2}] == "ma"]; then
-  echo 'build -> master'
+# TRAVIS_PULL_REQUEST=false
+# TRAVIS_PULL_REQUEST_BRANCH="develop"
+# TRAVIS_BRANCH="develop"
+export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST"=="false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
+
+build_project()
+{
   xcodebuild -workspace TravisTesting/TravisTesting.xcworkspace -scheme TravisTesting -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO | xcpretty
   xcodebuild test -workspace TravisTesting/TravisTesting.xcworkspace -scheme TravisTestingTests -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.3' -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO | xcpretty
   xcodebuild test -workspace TravisTesting/TravisTesting.xcworkspace -scheme TravisTestingUITests -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.3' -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO | xcpretty
+}
 
+if [[ "${BRANCH}" == "master" ]] || [[ "${BRANCH}" == "develop" ]] ; then
+  echo "build -> "${BRANCH}""
+  build_project
 fi
